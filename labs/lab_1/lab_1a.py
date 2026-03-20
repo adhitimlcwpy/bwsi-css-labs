75a80caa92407382a7f42a5f93900ac6769f39ef
Adhiti Murthy <theadhitimurthy@gmail.com>
2026-03-20 18:04:56 -0500

    Update lab_1a.py
    introduction added

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,13 @@ def main():
     name = "Adhiti Murthy" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    
+    print("Hi! My name is Adhiti Murthy. I am a sophomore from Texas. I am interested in physics and ML/AI. " \
+    "I have worked on a couple of independent research projects over the years at the intersection of ML with " \
+    "bioinformatics, epigenetics, and quantum computing. I'm also enrolled in embedded security and hacking " \
+    "as well as quantum software development and hope to grow my knowledge through these courses and gain " \
+    "some skills to apply to my future research. Apart from that, I play competitive badminton and love " \
+    "photography. Nice to meet everyone! :))")
 
 if __name__ == "__main__":
     main()
